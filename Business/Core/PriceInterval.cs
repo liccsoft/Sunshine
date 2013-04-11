ce59ac79e75d5967107172503c1514a934b09b4c
wangzheng <wz19841225@126.com>
2013-04-11 15:27:25 +0800

    tmp

---
+++ Business/Core/PriceInterval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sunshine.Business.Core
+{
+    [Table("PriceInterval")]
+    public class PriceInterval
+    {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int PriceIntervalId { get; set; }
+
+        [Display(Name = "价格区间")]
+        public string PriceIntervalName { get; set; }
+    }
+}
