f32fae2ca8556f8a5e13e7e7452203480d575518
liccsoft <liccsoft@outlook.com>
2013-04-18 19:51:07 +0800

    base controller

---
+++ Business/Agencies/ICommerceAgency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sunshine.Business.Core;
+
+namespace Sunshine.Business.Agencies
+{
+    public interface ICommerceAgency
+    {
+        Company RegisterCompany(Company company);
+        Company UpdateCompany(Company company);
+    }
+}
