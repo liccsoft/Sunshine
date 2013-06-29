b9e232ec80490270c8c765ebc9f0a460d8a3f887
liccsoft <liccsoft@outlook.com>
2013-06-29 11:21:19 +0800

    pagesize

--- Web/Controllers/ListController.cs
+++ Web/Controllers/ListController.cs
@@ -26,9 +26,9 @@ namespace Sunshine.Controllers
         //
         // GET: /Merchant/Create
 
-        public ActionResult Goods(string name, int? pageIndex, string field = "companyname")
+        public ActionResult Goods(string name, int? pageIndex, int? pageSize, string field = "companyname")
         {
-            var results = SearchEngine.Search(name, field ?? "companyname", "product", new  SearchOptions{ CurrentIndex = pageIndex ?? 1});
+            var results = SearchEngine.Search(name, field ?? "companyname", "product", new SearchOptions { CurrentIndex = pageIndex ?? 1, PageSize = pageSize??10 });
             ViewBag.Name = name;
             return View(results);
         }
